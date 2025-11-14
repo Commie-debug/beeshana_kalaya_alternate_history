701397e3fef1109dd2c46008f84536afc97be7a6
Commie45 <rithik.karadiyaan@icloud.com>
2025-11-14 17:19:56 +0530

    Create data.js

---
+++ out/html/data.js
@@ -0,0 +1,9 @@
+const tooltipList = [{
+    searchString: "Sri Lanka Armed Forces",
+    explanationText: "<img src=img/temp_party_symbols.png> The Socialist Party of Chile. <br> (Partido Socialista de Chile)"
+    }, 
+    {
+    searchString: "placeholder",
+    explanationText: "<img src=img/ps_a.png> Text here"
+    }
+];
