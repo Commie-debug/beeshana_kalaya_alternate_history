fd3334ee41feab7f195f4ff8edf09795e7dc9e08
Commie45 <rithik.karadiyaan@icloud.com>
2025-11-15 17:10:48 +0530

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,9 +1,5 @@
 const tooltipList = [{
     searchString: "Sri Lanka Armed Forces",
     explanationText: "<img src=img/temp_party_symbols.png> Sri Lanka Armed Forces"
-    }, 
-    {
-    searchString: "placeholder",
-    explanationText: "<img src=img/ps_a.png> Text here"
     }
 ];
