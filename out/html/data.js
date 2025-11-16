d2b1bf1612f877c2ed544d396dc035eaf5111ce9
Commie45 <rithik.karadiyaan@icloud.com>
2025-11-16 12:29:10 +0530

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -2,9 +2,9 @@ const colourList = [{
         word: "Sri Lanka Armed Forces",
         style: "color: #BA0000; font-weight: bold;",
         img: "img/temp_party_symbols.png"
-    },
-        word: "Sri Lanka Police",
-        style: "color: #BA0000; font-weight: bold;",
-        img: "img/temp_party_symbols.png"
     }
 ];
+const tooltipList = [{
+    searchString: "Sri Lanka Police",
+    explanationText: "<img src=img/temp_party_symbols.png> Sri Lanka Police"
+}];
