6ef47fd0c5e40e511250bf4381760c4baf54b51c
Commie45 <rithik.karadiyaan@icloud.com>
2025-11-16 18:17:55 +0530

    Update data.js

--- out/html/data.js
+++ out/html/data.js
@@ -1,10 +1,13 @@
 const colourList = [{
         word: "Sri Lanka Armed Forces",
-        style: "color: #002FA7; font-weight: bold;"
+        style: "color: #002FA7; font-weight: bold;",
+        img: "img src=img/logos/SLAF_Logo.png"
     },
         word: "SLFP",
-        style: "color: #002FA7; font-weight: bold;"
-}];
+        style: "color: #00008B; font-weight: bold;",
+        img: "img src=img/logos/SLFP_Logo.png"
+    }
+];
 
 const tooltipList = [{
     searchString: "Sri Lanka Armed Forces",
@@ -13,4 +16,7 @@ const tooltipList = [{
     {
     searchString: "SLFP",
     explanationText: "<img src=img/logos/SLFP_Logo.png> Sri Lanka Freedom Party"
-}];
+    }
+];
+
+
