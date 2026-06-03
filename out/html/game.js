cb68626715cb001eceaeddfbf8296be15b132457
Commie45 <rithik.karadiyaan@icloud.com>
2026-06-03 21:21:16 +0530

    aaaaaaaaa

--- out/html/game.js
+++ out/html/game.js
@@ -425,6 +425,27 @@
     }
   };
 
+  // POLL DISPLAY GRACIOUSLY PROVIDED BY FRANCOGAMER ON DISCORD 
+  scrollhor = function (tableid) {
+    (function() {
+        function scrollHorizontally(e) {
+            e = window.event || e;
+            var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+            this.scrollLeft -= (delta * 40); // Multiplied by 40
+            e.preventDefault();
+        }
+        if (document.getElementById(tableid).addEventListener) {
+            // IE9, Chrome, Safari, Opera
+            document.getElementById(tableid).addEventListener('mousewheel', scrollHorizontally, false);
+            // Firefox
+            document.getElementById(tableid).addEventListener('DOMMouseScroll', scrollHorizontally, false);
+        } else {
+            // IE 6/7/8
+            document.getElementById(tableid).attachEvent('onmousewheel', scrollHorizontally);
+        }
+    })();
+  }
+
   
   // This function allows you to modify the text before it's displayed.
   window.displayText = function (text) {
